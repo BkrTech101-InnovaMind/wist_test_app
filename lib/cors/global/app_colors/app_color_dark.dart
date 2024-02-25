1e72048cff8a9b7cf9e766fb53a68b4449647af5
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-02-25 06:31:33 +0300

    create the app colors for the tow modes

---
+++ lib/cors/global/app_colors/app_color_dark.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColorsDark {
+  static const primaryColor = Color(0xFFFFFFFF);
+  static const secondaryColor = Color(0xFFA2A2A7);
+  static const backGroundColor = Color(0xFF161622);
+  static const buttonsColor = Color(0xFF0066FF);
+  static const alarmColor = Color(0xFFEB0A24);
+}
---
+++ lib/cors/global/app_colors/app_color_light.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColorsLight {
+  static const primaryColor = Color(0xFF1E1E2D);
+  static const secondaryColor = Color(0xFFA2A2A7);
+  static const backGroundColor = Color(0xFFFFFFFF);
+  static const buttonsColor = Color(0xFF0066FF);
+  static const alarmColor = Color(0xFFEB0A24);
+}
