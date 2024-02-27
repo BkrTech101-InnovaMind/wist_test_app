e8efd310b9edb8cc1ccbe3e6c39861428e4aafae
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-02-27 11:59:19 +0300

    change the default screen to be the boarding instead of home

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen>
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
 
     Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacementNamed(context, "home");
+      Navigator.pushReplacementNamed(context, "boarding");
     });
   }
 
