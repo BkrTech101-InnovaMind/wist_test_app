654371bda5b41e010f9d987ca383bef8146ae8c3
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-02-27 12:01:26 +0300

    create a custom text to use for all the app

---
+++ lib/components/custom_text.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:wist_test_task/cors/global/app_colors/app_color_dark.dart';
+import 'package:wist_test_task/layout/_responsive_layout.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final FontWeight? fontWeight;
+  final double? fontSize;
+  final Color? color;
+  const CustomText(
+      {super.key,
+      required this.text,
+      this.fontWeight,
+      this.color,
+      this.fontSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: ResponsiveLayout.heightMultiplier(context, fontSize ?? 1.7),
+        fontWeight: fontWeight ?? FontWeight.normal,
+        color: color ?? AppColorsDark.primaryColor,
+      ),
+    );
+  }
+}
