e2b9d24b6a01a86c1921e2b93716457b58229c6c
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-02-28 08:47:06 +0300

    change some needed design

--- lib/components/page_indecator.dart
+++ lib/components/page_indecator.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:wist_test_task/cors/global/app_colors/app_color_dark.dart';
 import 'package:wist_test_task/layout/_responsive_layout.dart';
@@ -11,24 +13,25 @@ class PageIndecator extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: List.generate(
-          3,
-          (index) => Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 5),
-                child: AnimatedContainer(
-                  curve: Curves.easeIn,
-                  duration: Duration(milliseconds: 500),
-                  width: index == currentValue
-                      ? ResponsiveLayout.widthMultiplier(context, 10)
-                      : ResponsiveLayout.widthMultiplier(context, 4),
-                  height: ResponsiveLayout.heightMultiplier(context, 2),
-                  decoration: BoxDecoration(
-                    color: index == currentValue
-                        ? AppColorsDark.buttonsColor
-                        : AppColorsDark.secondaryColor,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              )),
+        3,
+        (index) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5),
+          child: AnimatedContainer(
+            curve: Curves.easeIn,
+            duration: const Duration(milliseconds: 400),
+            width: index == currentValue
+                ? ResponsiveLayout.widthMultiplier(context, 10)
+                : ResponsiveLayout.widthMultiplier(context, 3),
+            height: ResponsiveLayout.heightMultiplier(context, 1.5),
+            decoration: BoxDecoration(
+              color: index == currentValue
+                  ? AppColorsDark.buttonsColor
+                  : AppColorsDark.secondaryColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
